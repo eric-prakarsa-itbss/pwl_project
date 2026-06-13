d0ddcf2a20a30249fe084a2e7f0c2a3ea7b60b30
Eric Prakarsa Putra <shiro.noname@gmail.com>
2026-06-13 18:12:45 +0700

    Login and Register

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KRSController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\DosenController;
@@ -12,14 +13,20 @@ Route::get('/', function () {
     return view('dashboard');
 })->name('dashboard');
 
-Route::resource('/mahasiswa', MahasiswaController::class);
-Route::resource('/dosen', DosenController::class);
-Route::resource('/matakuliah', MataKuliahController::class);
-Route::resource('/jurusan', JurusanController::class);
-Route::resource('/kelas', KelasController::class);
-Route::resource('/krs', KRSController::class);
+Route::middleware('auth')->group(function(){
+    Route::resource('/mahasiswa', MahasiswaController::class);
+    Route::resource('/dosen', DosenController::class);
+    Route::resource('/matakuliah', MataKuliahController::class);
+    Route::resource('/jurusan', JurusanController::class);
+    Route::resource('/kelas', KelasController::class);
+    Route::resource('/krs', KRSController::class);
+});
 
+Route::get('/register', [AuthController::class, 'registerView']);
+Route::post('/register', [AuthController::class, 'register'])->name('register');
 
+Route::get('/login', [AuthController::class, 'loginView']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 // Route::get      => Get Data     => R => select
 // SELECT ALL   /   SELECT SPESIFIK
